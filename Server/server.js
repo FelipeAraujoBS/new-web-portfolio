9c74b644c1a107e01164c912389341017ad0b700
Felipe de Araujo <felipedearaujobs@hotmail.com>
2022-09-16 12:57:20 -0300

    second commit

--- Server/server.js
+++ Server/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+require('dotenv').config()
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
