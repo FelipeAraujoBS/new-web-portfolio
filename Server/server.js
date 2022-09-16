81b747306c152f371a5dd5d245c96ff9fba03afd
Felipe de Araujo <felipedearaujobs@hotmail.com>
2022-09-16 15:31:23 -0300

    third commit

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
