d60cebc1adf46994babf6caac1809af1ede4de43
Felipe de Araujo <felipedearaujobs@hotmail.com>
2022-09-17 00:50:29 -0300

    second commit

--- Client/src/components/Contact.js
+++ Client/src/components/Contact.js
@@ -28,7 +28,7 @@ const Contact = () => {
     e.preventDefault();
 
     setButtonText('Enviando...')
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://nodemailerscript.herokuapp.com/contact", {
       method: 'POST',
       headers: {
         "Content-Type": "application/json;charset=utf-8"
--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
+app.use("/post", router);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
