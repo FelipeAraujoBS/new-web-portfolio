14f891d5c3a26adb4682aa44ab3bdbdbd218c92c
Felipe de Araujo <felipedearaujobs@hotmail.com>
2022-09-25 21:06:45 -0300

    seventh commit

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config()
 
-// server used to send send emails
+// server used to send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
